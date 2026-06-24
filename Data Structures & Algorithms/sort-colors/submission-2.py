3138d6e72d11c2fe3d12b803ea734748a2917f2e
Mert Isik <mertisik329@gmail.com>
2026-06-23 19:52:24 -0700

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        buckets=[0,0,0]
+        for num in nums:
+            buckets[num]+=1
+        i=0
+        for j in range(len(buckets)):
+            for k in range(buckets[j]):
+                nums[i] = j
+                i+=1
