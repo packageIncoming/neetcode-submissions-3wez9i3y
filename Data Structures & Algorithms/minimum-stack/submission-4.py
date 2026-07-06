ad68ffe24d63c8edf72f2430df97cd53242bc3d7
Mert Isik <mertisik329@gmail.com>
2026-07-05 17:11:37 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,25 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mins = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.mins) ==0:
+            self.mins.append(val)
+        else:
+            if val < self.mins[-1]:
+                self.mins.append(val)
+            else:
+                self.mins.append(self.mins[-1])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.mins.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.mins[-1]
